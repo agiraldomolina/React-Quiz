2a659c6282284eccabf102513ff5361687f0420c
Alba Giraldo <agiraldomolina@gmail.com>
2024-10-25 15:38:13 -0400

    Loading questions form a fake API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+    questions: [],
+
+    // 'loading', 'error', 'ready', 'active', 'finished'
+    status: 'loading',
+};
+
+function reducer(state, action){
+    switch(action.type){
+        case 'dataReceived':
+            return {
+                ...state,
+                questions: action.payload,
+                status: 'ready',
+            };
+        case 'dataFailed':
+            return {
+                ...state,
+                status: 'error',
+                questions: [],
+            }
+        default:
+            throw new Error('Unknown action');
+    }
+}
+
 export default function App(){
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(function(){
+        fetch('http://localhost:8000/questions')
+        .then(response => response.json())
+        .then(data => dispatch({type: 'dataReceived', payload: data}))
+        .catch(error => dispatch({type: 'datafailed'}))
+    },[])
     return (
         <div className='app'>
             <Header />
